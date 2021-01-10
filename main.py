e2ef1ff545914784036ceb8a2573af03af20b887
Tuğçe Önder <tugceonder775@gmail.com>
2021-01-10 15:25:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import numpy as np
+#-----NUPY ORNEK 1---
+#python list
+#py_list =[1,2,3,4,5,6,7,8,9]
+#np array
+#np_array =([1,2,3,4,5,6,7,8,9])
+
+#py_multi=[[1,2,3,],[4,5,6],[7,8,9]]
+#np_multi=np_array.reshape(3,3)
+
+#print(np_multi)
+
+#-----NUMPY ORNEK 2---
+result = np.arange(1,10)
+result1= np.arange(10,1000,3)  #kendisi dizi oluşturuyor
+result2=np.linspace(0,100,5)# eşit aralıklar ile böler yani 5 eşit parçaya böler
+np_array =np.arange(50)
+result3=np_array.reshape(5,10)
+print(result)
+print()
+print(result1)
+print()
+print(result2)
+print()
+print(result3)
+
+
+
